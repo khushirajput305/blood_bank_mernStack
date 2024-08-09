8c175ab02bb946b1f32ed37e4814bd7135d14db5
khushirajput305 <khushinurawat305@gmail.com>
2024-08-09 12:45:09 +0530

    create schema

---
+++ models/userModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    require: [true, "role is required"],
+    enum: ["admin", "organisation", "user", "hospital"],
+  },
+  name: {
+    type: String,
+    required: function () {
+      if (this.role === "user" || this.role === "admin") {
+        return true;
+      }
+      return false;
+    },
+  },
+  organisationName:{
+    type: String,
+    required: function () {
+        if(this.role === 'organisation'){
+            return true
+        }
+        return false
+  }
+},
+hospitalName:{
+    type: String,
+    required: function () {
+        if(this.role === 'hospital'){
+            return true
+        }
+        return false
+  }
+},
+  email: {
+    type: String,
+    require: [true, "email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "password is required"],
+  },
+  website: {
+    type: String,
+  },
+  address: {
+    type: String,
+    require: [true, "address is required"],
+  },
+  phone: {
+    type: String,
+    require: [true, "phone number is required"],
+  },
+}, {timestamps:true});
+module.exports=mongoose.model('users', userSchema)
