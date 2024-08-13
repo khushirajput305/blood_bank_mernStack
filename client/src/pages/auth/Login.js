87695d2185215cc59a0ae41d6cb5f6a2ca692893
khushirajput305 <khushinurawat305@gmail.com>
2024-08-13 12:27:30 +0530

    login page

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
-  )
-}
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpg" alt="loginImage" />
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email here" className="form-label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password here" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputpassword"
+                aria-describedby="passwordHelp"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
