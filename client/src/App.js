58515ee58c72875fbcee1a06ea89a664dc99e922
khushirajput305 <khushinurawat305@gmail.com>
2024-08-26 12:34:15 +0530

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -1,13 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import API from "../../../services/API";
+import { toast } from "react-toastify";
 
 export const userLogin = createAsyncThunk(
-    'auth/login',
-    async({role,email,password},{rejectWithValue})=>{
-        try {
-            
-        } catch (error) {
-            
-        }
-
+  "auth/login",
+  async ({ role, email, password }, { rejectWithValue }) => {
+    try {
+      const { data } = await API.post("/auth/login", { role, email, password });
+      //store token
+      if (data.success) {
+        localStorage.setItem("token", data.token);
+        toast.success(data.message);
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
     }
-)
+  }
+);
