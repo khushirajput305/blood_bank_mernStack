210955defd1003c6e4aa2e7120a07ffc205749c1
khushirajput305 <khushinurawat305@gmail.com>
2024-08-28 18:51:08 +0530

    logout functionality

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Form from "../../components/shared/Form/Form";
+import { useSelector } from "react-redux";
+import Spinner from "../../components/shared/Spinner";
+
+
 const Login = () => {
+  const { loading, error } = useSelector((state) => state.auth);
   return (
     <>
-      <div className="row g-0">
-        <div className="col-md-8 form-banner1">
-          <img src="./assets/images/blood-Donation.jpg" alt="loginImage" />
+    {error && <span>{ alert(error)}</span>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="row g-0">
+          <div className="col-md-8 form-banner1">
+            <img src="./assets/images/blood-Donation.jpg" alt="loginImage" />
+          </div>
+          <div className="col-md-4 form-container">
+            <Form
+              formTitle={"Login Page"}
+              submitBtn={"login"}
+              formType={"login"}
+            />
+          </div>
         </div>
-        <div className="col-md-4 form-container">
-          <Form formTitle={'Login Page'} submitBtn={'login'} formType={'login'}/>
-        </div>
-      </div>
+      )}
     </>
   );
 };
