cdb3e93f23dd4216c9fe534157041c2f5f02c6bb
khushirajput305 <khushinurawat305@gmail.com>
2024-08-28 13:47:52 +0530

    add some new feature

--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -12,6 +12,7 @@ export const userLogin = createAsyncThunk(
       if (data.success) {
         localStorage.setItem("token", data.token);
         toast.success(data.message);
+        window.location.replace("/");
       }
       return data;
     } catch (error) {
@@ -54,9 +55,9 @@ export const userRegister = createAsyncThunk(
         website,
       });
       if (data?.success) {
-        alert("User Registerd Successfully");
+         alert("User Registerd Successfully");
         window.location.replace("/login");
-        // toast.success("User Registerd Successfully");
+         //toast.success(data.message);
       }
     } catch (error) {
       console.log(error);
@@ -68,3 +69,23 @@ export const userRegister = createAsyncThunk(
     }
   }
 );
+
+//current user
+export const getCurrentUser = createAsyncThunk(
+  'auth/getCurrentUser',
+  async({rejectWithValue})=>{
+    try {
+      const res = await API.get('/auth/current-user')
+      if(res?.data){
+        return res?.data;
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      } 
+    }
+  }
+)
