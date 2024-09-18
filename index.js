c3789b288c21aba54df0b8ebda742f90f2ed619d
khushirajput305 <khushinurawat305@gmail.com>
2024-09-18 18:10:33 +0530

    completion

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -15,12 +16,21 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/v1/test", require("./routes/testRoutes"));
-app.use("/api/v1/auth",require("./routes/authRoutes"));
-app.use("/api/v1/inventory",require("./routes/inventoryRoutes"));
-app.use("/api/v1/analytics",require("./routes/analyticsRoutes"));
-app.use("/api/v1/admin",require("./routes/adminRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-const PORT = process.env.PORT || 8000
+//Static folder
+app.use(express.static(path.join(__dirname, "./client/build")));
+//static routes
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`server is running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`.bgBlue.white);
+  console.log(
+    `server is running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
